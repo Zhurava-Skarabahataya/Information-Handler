442155fa67245d858c98753ce103b06a0eb643d6
Zhurava-Skarabahataya <zhurova1994@gmail.com>
2020-07-28 21:51:52 +0300

    re

---
+++ src/by/epamtc/textonator/logic/TextRestorer.java
@@ -0,0 +1,24 @@
+package by.epamtc.textonator.logic;
+
+public class TextRestorer {
+
+	private static final TextRestorer instance = new TextRestorer();
+	
+	private TextRestorer(){
+		
+	}
+	
+	public static TextRestorer getInstance() {
+		return instance;
+	}
+	
+	public String restoreText() {
+		
+		StringBuilder text = new StringBuilder();
+		
+		//доделаю в среду...
+		
+		return text.toString();
+	}
+	
+}
